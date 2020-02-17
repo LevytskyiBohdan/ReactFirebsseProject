7caf0b69bed5f82be2312490bc898664356d6b03
Mykola Prymak <mprymak@eagleinvsys.com>
2020-02-17 17:57:55 +0200

    add img to About

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Route } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
+import '../css/about.css';
 
 class About extends React.Component {
     render() {
         return (<>
-            <p>About</p>
+            <div className="container-fluid about">
+                <div className="row">
+                    <div className="col-12 top"></div>
+                </div>
+                <div className="row">
+                    
+                </div>
+            </div>
         </>)
     }
 }
