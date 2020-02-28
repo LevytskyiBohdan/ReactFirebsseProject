f130f11a876d0731cf7db3c9858b37dd3b73d117
Bohdan Levytskyi <levytskyibohdan@gmail.com>
2020-02-28 12:37:54 +0200

    add cloud function that allow change author in posts

--- firebase-users/functions/index.js
+++ firebase-users/functions/index.js
@@ -1,21 +1,29 @@
+/* eslint-disable promise/no-nesting */
 /* eslint-disable promise/always-return */
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBivmREHCYLY5UQd70Nn2iLE1ilUL2OQ7Q",
-    authDomain: "react-firebase-project-f71c4.firebaseapp.com",
-    databaseURL: "https://react-firebase-project-f71c4.firebaseio.com",
-    projectId: "react-firebase-project-f71c4",
-    storageBucket: "react-firebase-project-f71c4.appspot.com",
-    messagingSenderId: "860315756739",
-    appId: "1:860315756739:web:a04429374fdcdbe6505a76",
-    measurementId: "G-H0SWCSLBKX"
-  };
+  apiKey: "AIzaSyBivmREHCYLY5UQd70Nn2iLE1ilUL2OQ7Q",
+  authDomain: "react-firebase-project-f71c4.firebaseapp.com",
+  databaseURL: "https://react-firebase-project-f71c4.firebaseio.com",
+  projectId: "react-firebase-project-f71c4",
+  storageBucket: "react-firebase-project-f71c4.appspot.com",
+  messagingSenderId: "860315756739",
+  appId: "1:860315756739:web:a04429374fdcdbe6505a76",
+  measurementId: "G-H0SWCSLBKX"
+};
 
 admin.initializeApp(firebaseConfig);
 
+firebase.initializeApp(firebaseConfig);
+
 const auth = admin.auth();
+const db = firebase.firestore();
 
 /*
  * Gets all the users (1000 MAX) from Firebase auth.
@@ -24,24 +32,64 @@ const auth = admin.auth();
  * @param {Object} res Express Response Object
  */
 const getAllUsers = (req, res) => {
-  const maxResults = 1; // optional arg.
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.set('Access-Control-Allow-Origin', '*');
 
   auth.listUsers().then((userRecords) => {
     let users = [];
 
     userRecords.users.forEach((user) => {
-        users.push(user.toJSON())
+      users.push(user.toJSON())
     });
     res.send(users);
-  }).catch((error) => { 
-      console.log(error)
-      res.send(error);
-    });
+  }).catch((error) => {
+    console.log(error)
+    res.send(error);
+  });
 };
+
+const changePostsAutor = (req, res) => {
+  new Promise((resolve, reject) => {
+    db.collection('posts').where('owner', '==', req.body.owner)
+      .get()
+      .then((querySnapshot) => {
+        if (querySnapshot.docs.length !== 0) {
+
+          querySnapshot.forEach((doc) => {
+            let data = [];
+            querySnapshot.forEach((doc) => {
+              data.push(doc.id);
+            });
+
+            return resolve(data)
+          });
+        }
+        return resolve([]);
+      })
+      .catch((error) => {
+        return reject(error)
+      });
+
+  }).then(data => {
+    data.forEach((post, idx) => {
+      db.collection('posts').doc(post).set({
+        author: req.body.newName,
+      }, { merge: true })
+        .then(() => {
+          if ((data.length - 1) === idx) res.send("all ok, >>>>")
+        })
+        .catch((error) => {
+          res.send({ error, dsdf: "sdasd" })
+        });
+
+    })
+  })
+    .catch(err => res.send(err));
+}
+
 
 module.exports = {
   api: functions.https.onRequest(getAllUsers),
+  changePostsAutor: functions.https.onRequest(changePostsAutor),
 };
 
 // Create and Deploy Your First Cloud Functions
