367edb72fa2200e9465e8e243c36c9593fe95f7e
Bohdan Levytskyi <levytskyibohdan@gmail.com>
2020-03-02 13:36:09 +0200

    rewrite App component to functional component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-// import * as userActions from './actions/user';
+import {
+  BrowserRouter,
+} from "react-router-dom";
 import Header from './components/Header';
 import ModalView from './views/ModalView';
 import routes from './router'
@@ -9,33 +9,22 @@ import './App.css';
 import Loader from './components/Loader';
 
 import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'connected-react-router';
 import configureStore, { history } from './store/index';
 
 const store = configureStore();
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Provider store={store}>
-          <ConnectedRouter history={history}>
-
-            <Loader>
-              <ModalView />
-              <Header />
-              {routes}
-            </Loader>
-
-          </ConnectedRouter>
-        </Provider>
-      </>
-    );
-  }
-};
-
-// const mapDispatchToProps = dispatch => ({
-//   userActions: bindActionCreators(userActions, dispatch),
-// });
+const App = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter history={history}>
+        <Loader>
+          <ModalView />
+          <Header />
+          {routes}
+        </Loader>
+      </BrowserRouter>
+    </Provider>
+  );
+}
 
 export default App;
