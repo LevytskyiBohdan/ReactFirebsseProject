44113e2625b87cfe545e74bc60651b6aaf093fb2
Bohdan Levytskyi <levytskyibohdan@gmail.com>
2020-03-03 10:46:37 +0200

    InfoMessage with hooks

--- src/components/InfoMessage.js
+++ src/components/InfoMessage.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class InfoMessage extends React.Component {
-    render() {
-        const message = this.props.message;
-        return (message &&
-            <>
-                <div className="alert alert-success" role="alert">
-                    <p>{message}</p>
-                </div>
-            </>) || null;
-    }
+const InfoMessage = ({message}) => {
+    return (message &&
+        <>
+            <div className="alert alert-success" role="alert">
+                <p>{message}</p>
+            </div>
+        </>) || null;
 }
+
 
 InfoMessage.propTypes = {
     message: PropTypes.any,
