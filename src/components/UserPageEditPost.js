31cf8de2d1b7815de174c77c9c69aab715d72be0
Bohdan Levytskyi <levytskyibohdan@gmail.com>
2020-03-04 13:19:14 +0200

    UserPageEditPost with hooks

--- src/components/UserPageEditPost.js
+++ src/components/UserPageEditPost.js
@@ -4,166 +4,131 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { push } from 'connected-react-router';
-import * as userActions from '../actions/user';
 import * as postsActions from '../actions/posts';
 import * as postActions from '../actions/post';
-import * as modalActions from '../actions/modal';
 import * as fileUploadActions from '../actions/fileUpload';
-import FileUploader from './FileUploader';
 import InfoMessage from './InfoMessage';
-import ErrorMessage from './ErrorMessage';
-import { CREATE_POST_SUCCESS, CREATE_POST_FAILURE } from '../constants';
+import FileUploader from './FileUploader';
+import { EDIT_POST_SUCCESS, GET_POST_SUCCESS, EDIT_POST_FAILURE } from '../constants/index';
 
-class UserPageEditPost extends React.Component {
-    constructor(props) {
-        super(props);
+const UserPageEditPost = ({ currentStoreStatus, post, postActions, postsActions, postId, chosenFiles, fileUploadActions }) => {
+    const [title, setTitle] = React.useState();
+    const [article, setArticle] = React.useState('');
+    const [publish, setPublish] = React.useState('');
+    const [isCreating, setIsCreating] = React.useState(false);
+    const [isMessage, setIsMessage] = React.useState(false);
 
-        this.state = {
-            title: '',
-            article: '',
-            clearUploader: false,
-            publish: false,
-            isCreating: false,
-        }
-    }
+    React.useEffect(() => {
+        postActions.getPost("posts", postId)
+    }, [])
 
-    componentDidMount() {
-        this.props.postActions.getPost("posts", this.props.postId)
-    }
-
-    componentDidUpdate(prevProps) {
-        const post = this.props.post.postById;
-
-        if (post && post !== prevProps.post.postById) {
-            this.setState({
-                title: post.title,
-                article: post.article,
-                publish: post.publish,
-                img: post.img || [],
-            })
-
-            this.props.fileUploadActions.setChosenFiles(post.img)
+    React.useEffect(() => {
+        if (currentStoreStatus === EDIT_POST_SUCCESS) {
+            fileUploadActions.clearFileUploader();
+            setIsCreating(false);
+            setIsMessage(true);
         }
 
-        if (!this.props.posts.error &&
-            this.props.posts !== prevProps.posts &&
-            this.props.posts.isLoaded) {
-                
-            this.props.fileUploadActions.clearFileUploader();
-
-            this.props.push('/user/myPosts')
-        }
-    }
-
-    onSubmit() {
-        if (this.state.isCreating) {
-            return null;
+        if (currentStoreStatus === GET_POST_SUCCESS) {
+            setTitle(post.postById.title);
+            setArticle(post.postById.article);
+            setPublish(post.postById.publish);
+            fileUploadActions.setChosenFiles(post.postById.img || []);
         }
 
-        this.setState({
-            isCreating: true,
-        })
+        if (currentStoreStatus === EDIT_POST_FAILURE) setIsCreating(false);
+    }, [currentStoreStatus, fileUploadActions, post.postById])
 
+
+    function onSubmit() {
+        if (isCreating) return null;
+
+        setIsCreating(true);
 
         const date = {
-            title: this.state.title,
-            img: this.props.chosenFiles,
-            article: this.state.article,
-            publish: this.state.publish,
+            title,
+            img: chosenFiles,
+            article,
+            publish,
         }
-
-        this.props.postsActions.editPost('posts', this.props.postId, date);
+        postsActions.editPost('posts', postId, date)
 
     }
 
-    showStatusMessage() {
-        switch (this.props.posts.status) {
-            case CREATE_POST_SUCCESS:
-                return <InfoMessage message="Post was created." />;
-            case CREATE_POST_FAILURE:
-                return <ErrorMessage error={this.props.posts.error} />
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h3 className="mt-3">Edit post</h3>
 
-        }
-    }
+                    {isMessage && <InfoMessage message="Post was edited." />}
+                    
+                    {post.postById ?
+                        (
+                            <form className="mt-4">
+                                <div className="form-group">
+                                    <label htmlFor="title">Title</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="title"
+                                        value={title}
+                                        onChange={evt => { setTitle(evt.target.value) }}
+                                    />
+                                </div>
 
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h3 className="mt-3">Edit post</h3>
-                        {this.props.post.postById ?
-                            (
-                                <form className="mt-4">
-                                    {this.showStatusMessage()}
-                                    <div className="form-group">
-                                        <label htmlFor="title">Title</label>
-                                        <input
-                                            type="text"
-                                            className="form-control"
-                                            id="title"
-                                            value={this.state.title}
-                                            onChange={evt => { this.setState({ title: evt.target.value }) }}
-                                        />
+                                <div className="mb-3">
+                                    <label htmlFor="validationTextarea">Article</label>
+                                    <textarea
+                                        className="form-control"
+                                        placeholder="text..."
+                                        value={article}
+                                        onChange={evt => { setArticle(evt.target.value) }}
+                                        style={{ height: "35vh" }}
+                                    ></textarea>
+
+                                </div>
+
+                                <div className="form-group">
+                                    <div className="custom-file">
+                                        <FileUploader />
                                     </div>
+                                </div>
 
-                                    <div className="mb-3">
-                                        <label htmlFor="validationTextarea">Article</label>
-                                        <textarea
-                                            className="form-control"
-                                            placeholder="text..."
-                                            value={this.state.article}
-                                            onChange={evt => { this.setState({ article: evt.target.value }) }}
-                                            style={{ height: "35vh" }}
-                                        ></textarea>
+                                <div className="form-group form-check">
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input"
+                                        checked={publish}
+                                        id="publish"
+                                        onChange={() => { setPublish(!publish) }}
+                                    />
+                                    <label className="form-check-label" htmlFor="publish">Publish post</label>
+                                </div>
 
-                                    </div>
+                                <div className="form-group">
+                                    <button
+                                        type="button"
+                                        className="btn btn-block mt-3 btn-success"
+                                        disabled={isCreating}
+                                        onClick={() => { onSubmit() }}
+                                    >Save</button>
+                                </div>
+                            </form>
 
-                                    <div className="form-group">
-                                        <div className="custom-file">
-                                            <FileUploader />
-                                        </div>
-                                    </div>
+                        ) :
+                        (<h6>Loading...</h6>)
+                    }
 
-                                    <div className="form-group form-check">
-                                        <input
-                                            type="checkbox"
-                                            className="form-check-input"
-                                            checked={this.state.publish}
-                                            id="publish"
-                                            onChange={() => {
-                                                this.setState({
-                                                    publish: !this.state.publish,
-                                                })
-                                            }}
-                                        />
-                                        <label className="form-check-label" htmlFor="publish">Publish post</label>
-                                    </div>
-
-                                    <div className="form-group">
-                                        <button
-                                            type="button"
-                                            className="btn btn-block mt-3 btn-success"
-                                            disabled={this.state.isCreating}
-                                            onClick={() => { this.onSubmit() }}
-                                        >Save</button>
-                                    </div>
-                                </form>
-
-                            ) :
-                            (<h6>Loading...</h6>)
-                        }
-
-                    </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 const mapStateToProps = (state, ownProps) => ({
+    currentStoreStatus: state.currentStoreStatus,
     postId: ownProps.match.params.id,
-    state,
     post: state.post,
     user: state.user.currentUser,
     posts: state.posts,
@@ -171,12 +136,9 @@ const mapStateToProps = (state, ownProps) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    push: path => dispatch(push(path)),
-    userActions: bindActionCreators(userActions, dispatch),
     postsActions: bindActionCreators(postsActions, dispatch),
     postActions: bindActionCreators(postActions, dispatch),
     fileUploadActions: bindActionCreators(fileUploadActions, dispatch),
-    modalActions: bindActionCreators(modalActions, dispatch),
 });
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserPageEditPost));
